5b4721f2ab3aa5245549f28c1c122635ce00989b
Sue-Holding <sue.holding55@gmail.com>
2025-03-19 14:22:02 +0100

    fixing test.ts

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,7 +1,8 @@
-import { SessionData } from "express-session";
+import { Cookie, SessionData } from "express-session";
 
 const testSession: SessionData = {
-  user: { name: "Test User" }
+  user: { name: "Test User" },
+  cookie: new Cookie
 };
 
 console.log(testSession.user?.name);
