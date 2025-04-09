3f9cd0fa6734142287222f126e8898b2c77aa52f
Sue-Holding <sue.holding55@gmail.com>
2025-04-09 14:50:36 +0200

    changed cors policy - server

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors(
-    {origin: "http://127.0.0.1:5500"}
+    {origin: "http://localhost:5173"}
 ));
 app.use(express.urlencoded({extended: true}));
 
