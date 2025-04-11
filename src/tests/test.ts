a884bef8e43f9e0ee56146d2b839b525ba284896
Sue-Holding <sue.holding55@gmail.com>
2025-04-11 11:32:31 +0200

    removed unneeded folder

--- src/tests/test.ts
+++
@@ -1,8 +0,0 @@
-import { Cookie, SessionData } from "express-session";
-
-const testSession: SessionData = {
-  user: { name: "Test User" },
-  cookie: new Cookie
-};
-
-console.log(testSession.user?.name);
