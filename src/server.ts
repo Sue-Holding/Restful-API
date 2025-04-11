8d78f24303c14b28162ca9718f0d61031c5eadfd
Sue-Holding <sue.holding55@gmail.com>
2025-04-11 11:10:59 +0200

    cors and session

--- src/server.ts
+++ src/server.ts
@@ -16,24 +16,24 @@ const PORT = process.env.PORT || 3000;
 const isProduction = process.env.NODE_ENV === 'production';
 
 // Middlewares
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://restful-api-animals.onrender.com'],
-  credentials: true
-}));
 // app.use(cors({
-//   origin: (origin, callback) => {
-//     const allowedOrigins = [
-//       'http://localhost:5173',
-//       'https://restful-api-animals.onrender.com'
-//     ];
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true // disable whilst no login function
+//   origin: ['http://localhost:5173', 'https://restful-api-animals.onrender.com'],
+//   credentials: true
 // }));
+app.use(cors({
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      'http://localhost:5173',
+      'https://restful-api-animals.onrender.com'
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // disable whilst no login function
+}));
 
 //body parsers
 app.use(express.urlencoded({extended: true}));
@@ -46,9 +46,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: { 
       secure: isProduction, // true only in production (HTTPS)
-    sameSite: isProduction ? 'none' : 'lax', 
-      // secure: process.env.NODE_ENV === 'production',
-      // sameSite: 'none',
+      sameSite: isProduction ? 'none' : 'lax', 
       httpOnly: true,
     }, 
   }));
